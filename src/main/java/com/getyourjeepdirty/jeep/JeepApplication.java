7a0097ffa79338dd28ed02da9b0c3538fd9745dc
peterlokey <peter.lokey1@gmail.com>
2020-03-13 15:59:33 -0500

    initial commit

---
+++ src/main/java/com/getyourjeepdirty/jeep/JeepApplication.java
@@ -0,0 +1,13 @@
+package com.getyourjeepdirty.jeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getyourjeepdirty/jeep/JeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getyourjeepdirty.jeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
