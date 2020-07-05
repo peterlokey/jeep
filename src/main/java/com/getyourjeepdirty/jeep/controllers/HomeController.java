4b69fb556c86c562ec97d5ead32d267a19344e7d
peterlokey <peter.lokey1@gmail.com>
2020-07-05 14:58:11 -0500

    Added terms of service, disclaimer at user-signup, FB and LinkedIn links, under-construction banner.

--- src/main/java/com/getyourjeepdirty/jeep/controllers/HomeController.java
+++ src/main/java/com/getyourjeepdirty/jeep/controllers/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -30,6 +31,11 @@ public class HomeController {
 
         model.addAttribute("eventList", sortedByDate());
         return "index";
+    }
+
+    @RequestMapping(value = "terms")
+    public String terms() {
+        return "terms";
     }
 
     public ArrayList<Event> sortedByDate() {
